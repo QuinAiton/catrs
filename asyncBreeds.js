2f144d2efdc0e1cb3ff8d0eb03e2226b3dc43285
QuintenAiton <quinaiton@hotmail.com>
2021-01-13 01:51:23 +0000

    gets information about cat breed synchronously

---
+++ asyncBreeds.js
@@ -0,0 +1,19 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function (breed, callBack) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) callBack(data);
+    if (error) callBack(undefined);
+  });
+};
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed);
+};
+
+
+// we try to get the return value
+breedDetailsFromFile('Bombay', printOutCatBreed);
+module.exports = breedDetailsFromFile;
